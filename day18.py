871a8b0699d471b7882c93185c998ce8a9e7cbe2
anamaria013 <topuriateo11@gmail.com>
2025-02-03 14:40:40 +0400

    commit

---
+++ day18.py
@@ -0,0 +1,31 @@
+for i in range(10):
+    print(i)
+
+
+
+for i in range(50,150):
+    print(i)
+
+
+
+for i in range(200,500,2):
+    print(i)
+
+
+
+for i in range(0,50,1):
+    print(i)
+
+
+
+for i in range(5,10,1):
+    print(i)
+
+
+
+name="anamaria"
+
+
+
+for i in  name:
+    print(i)
---
+++ day18hw.py
@@ -0,0 +1,14 @@
+i=0
+
+while i < 10 :
+    print("anamaria machaidze")
+    i=i+1
+
+for i in range(1,21):
+    print(i)
+
+for i in range (20,0,-1):
+    print(i)
+
+for i in range(1,50,+1):
+    print(i)
