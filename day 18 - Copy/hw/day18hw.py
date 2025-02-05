b692247a0db973f6c6aa3f70562cca2c8aff972f
anamaria013 <topuriateo11@gmail.com>
2025-02-05 18:26:20 +0400

    commit

---
+++ day 18 - Copy/hw/day18hw.py
@@ -0,0 +1,33 @@
+
+
+for _ in range(10):
+    print("anamaria machaidze")
+   
+
+for i in range(1,21):
+    print(i)
+
+for i in range (20,0,-1):
+    print(i)
+
+nam1=0
+
+for i in range(1,51):
+    nam1=nam1+i
+print(nam1)
+
+nam2=0
+
+for i in range(2,11,2):
+    nam2=nam2+i
+print(nam2)
+
+nam3=int(input("enter nam3 : "))
+
+for i in range(0,nam3+1):
+    if nam3%2==0:
+        print(str(nam3)+" is even")
+    else:
+        print(str(nam3)+" is odd")
+
+print(10_10)
